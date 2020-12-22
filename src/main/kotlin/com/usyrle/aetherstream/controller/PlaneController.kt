6635c179df76e6c9a7bb6e472094aa3dfbc587da
usyrle <awy6143@gmail.com>
2020-12-22 17:27:32 -0600

    expose endpoint to play next card of a given planar deck

--- src/main/kotlin/com/usyrle/aetherstream/controller/PlaneController.kt
+++ src/main/kotlin/com/usyrle/aetherstream/controller/PlaneController.kt
@@ -1,5 +1,6 @@
 package com.usyrle.aetherstream.controller
 
+import com.usyrle.aetherstream.repo.PlanarDeck
 import com.usyrle.aetherstream.repo.PlanarDeckRepository
 import com.usyrle.aetherstream.service.PlaneService
 import org.springframework.web.bind.annotation.*
@@ -35,6 +36,35 @@ class PlaneController(private val service: PlaneService, private val repo: Plana
                 currentPlane = requestedDeck.cards[requestedDeck.currentIndex],
                 startTime = requestedDeck.startTime.toInstant().epochSecond,
                 id = requestedDeck.id ?: "00000000"
+            )
+        }
+        return null
+    }
+
+    @PostMapping("/{deckId}/next")
+    fun playNextPlanarCard(
+        @PathVariable deckId: String
+    ): PlanarDeckInfo? {
+        val result = repo.findById(deckId)
+
+        if (result.isPresent) {
+            val deck = result.get()
+            val nextPlaneIndex = if (deck.currentIndex + 1 >= deck.cards.size) 0 else deck.currentIndex + 1
+
+            repo.save(
+                PlanarDeck(
+                    cards = deck.cards,
+                    startTime = deck.startTime,
+                    currentIndex = nextPlaneIndex,
+                    id = deck.id
+                )
+            )
+
+            return PlanarDeckInfo(
+                deckSize = deck.cards.size,
+                currentPlane = deck.cards[nextPlaneIndex],
+                startTime = deck.startTime.toInstant().epochSecond,
+                id = deck.id ?: "00000000"
             )
         }
         return null
--- src/test/kotlin/com/usyrle/aetherstream/controller/PlaneControllerTest.kt
+++ src/test/kotlin/com/usyrle/aetherstream/controller/PlaneControllerTest.kt
@@ -1,6 +1,7 @@
 package com.usyrle.aetherstream.controller
 
 import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doNothing
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import com.usyrle.aetherstream.repo.PlanarCard
@@ -10,12 +11,19 @@ import com.usyrle.aetherstream.service.PlaneService
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockito.InjectMocks
-import org.mockito.Mock
-import org.mockito.MockitoAnnotations
+import org.mockito.*
 import java.util.*
 
 internal class PlaneControllerTest {
+
+    val testCards = mutableListOf(
+        PlanarCard("Plane1", "plane", "https://api.scryfall.com", 12341),
+        PlanarCard("Plane2", "plane", "https://api.scryfall.com", 12342),
+        PlanarCard("Plane3", "plane", "https://api.scryfall.com", 12343)
+    )
+
+    @Captor
+    lateinit var deckCaptor: ArgumentCaptor<PlanarDeck>
 
     @Mock
     lateinit var mockPlaneService: PlaneService
@@ -32,9 +40,7 @@ internal class PlaneControllerTest {
 
         whenever(mockPlaneService.generatePlanarDeck(any(), any())).thenReturn(
             PlanarDeck(
-                cards = mutableListOf(
-                    PlanarCard("Plane1", "plane", "https://api.scryfall.com", 12341)
-                ),
+                cards = testCards,
                 startTime = Date()
             )
         )
@@ -56,11 +62,6 @@ internal class PlaneControllerTest {
 
     @Test
     fun generateNewPlanarDeck_returnsBasicInfoAboutDeck() {
-        val testCards = mutableListOf(
-            PlanarCard("Plane1", "plane", "https://api.scryfall.com", 12341),
-            PlanarCard("Plane2", "plane", "https://api.scryfall.com", 12342),
-            PlanarCard("Plane3", "plane", "https://api.scryfall.com", 12343)
-        )
         val testDeck = PlanarDeck(
             cards = testCards,
             startTime = Date(),
@@ -80,11 +81,6 @@ internal class PlaneControllerTest {
 
     @Test
     fun getPlanarDeckInfo_returnsBasicInfoAboutDeck() {
-        val testCards = mutableListOf(
-            PlanarCard("Plane1", "plane", "https://api.scryfall.com", 12341),
-            PlanarCard("Plane2", "plane", "https://api.scryfall.com", 12342),
-            PlanarCard("Plane3", "plane", "https://api.scryfall.com", 12343)
-        )
         val testDeck = PlanarDeck(
             cards = testCards,
             startTime = Date(),
@@ -104,6 +100,79 @@ internal class PlaneControllerTest {
 
     @Test
     fun getPlanarDeckInfo_returnsNullIfNotFound() {
+        whenever(mockRepository.findById("TEST")).thenReturn(Optional.empty())
+
+        val actual = subject.getPlanarDeckInfo("TEST")
+
+        assertThat(actual).isNull()
+    }
+
+    @Test
+    fun playNextPlanarCard_returnsBasicInfoAboutDeck() {
+        val testDeck = PlanarDeck(
+            cards = testCards,
+            startTime = Date(),
+            currentIndex = 0,
+            id = "TEST"
+        )
+
+        whenever(mockRepository.findById("TEST")).thenReturn(Optional.of(testDeck))
+        whenever(mockRepository.save(testDeck)).thenReturn(testDeck)
+
+        val actual = subject.playNextPlanarCard("TEST")
+
+        assertThat(actual!!.deckSize).isEqualTo(testCards.size)
+        assertThat(actual.startTime).isEqualTo(testDeck.startTime.toInstant().epochSecond)
+        assertThat(actual.currentPlane).isEqualTo(testCards[1])
+        assertThat(actual.id).isEqualTo(testDeck.id)
+    }
+
+    @Test
+    fun playNextPlanarCard_updatesDatabaseWithNewIndex() {
+        val testDeck = PlanarDeck(
+            cards = testCards,
+            startTime = Date(),
+            currentIndex = 0,
+            id = "TEST"
+        )
+
+        whenever(mockRepository.findById("TEST")).thenReturn(Optional.of(testDeck))
+
+        subject.playNextPlanarCard("TEST")
+
+        verify(mockRepository).save(deckCaptor.capture())
+
+        val actual = deckCaptor.value
+
+        assertThat(actual.cards).isEqualTo(testCards)
+        assertThat(actual.startTime).isEqualTo(testDeck.startTime)
+        assertThat(actual.currentIndex).isEqualTo(testDeck.currentIndex + 1)
+        assertThat(actual.id).isEqualTo(testDeck.id)
+    }
+
+    @Test
+    fun playNextPlanarCard_indexRollsOverWhenListEndIsReached() {
+        val testDeck = PlanarDeck(
+            cards = testCards,
+            startTime = Date(),
+            currentIndex = 2,
+            id = "TEST"
+        )
+
+        whenever(mockRepository.findById("TEST")).thenReturn(Optional.of(testDeck))
+
+        subject.playNextPlanarCard("TEST")
+
+        verify(mockRepository).save(PlanarDeck(
+            cards = testDeck.cards,
+            startTime = testDeck.startTime,
+            currentIndex = 0,
+            id = testDeck.id
+        ))
+    }
+
+    @Test
+    fun playNextPlanarCard_returnsNullIfNotFound() {
         whenever(mockRepository.findById("TEST")).thenReturn(Optional.empty())
 
         val actual = subject.getPlanarDeckInfo("TEST")
